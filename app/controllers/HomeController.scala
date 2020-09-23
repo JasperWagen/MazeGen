65d84d98e5635089a0ff1c005e9b8f417ab87098
JasperWagen <jasperwagen@outlook.com>
2020-09-23 12:23:33 +0100

    added docker file and changed mazeImg output method

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -35,6 +35,12 @@ class HomeController @Inject()(controllerComponents: ControllerComponents) exten
     )(MazeForm.apply)(MazeForm.unapply)
   )
 
+  implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
+
+  def serveImg(imgPath: String) = Action {
+    Ok.sendFile(new java.io.File("/"+imgPath))
+  }
+
   def newMaze(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     mazeForm.bindFromRequest().fold(
       formWithErrors => {
@@ -69,13 +75,15 @@ class HomeController @Inject()(controllerComponents: ControllerComponents) exten
           val searchPath = dfSearch.search(popCanvas)
           val searchArr = dfSearch.mapSearchPathToArr(searchPath, mazeDimensions)
           val solvedMazeImg = arrayToImage.mapArrayToImg(searchArr, mazeImg, 16711680, 20, 100F)
+          val tempMazeImg = File.createTempFile("mazeImg", ".png")
 
-          //ImageIO.write(solvedMazeImg, "png", new TemporaryFile("public/images/mazeBucket/TEST.png"))
-          Ok(views.html.index(mazeForm, modifiedMazeData, "nothing"))
+          ImageIO.write(mazeImg, "png", tempMazeImg)
+          Ok(views.html.index(mazeForm, modifiedMazeData, tempMazeImg.getAbsolutePath))
         }
-        //File.TemporaryFileCreator()
-        //ImageIO.write(mazeImg, "png", new File("public/images/mazeBucket/TEST.png"))
-        Ok(views.html.index(mazeForm, modifiedMazeData, "nothing"))
+        val tempMazeImg = File.createTempFile("mazeImg", ".png")
+        ImageIO.write(mazeImg, "png", tempMazeImg)
+
+        Ok(views.html.index(mazeForm, modifiedMazeData, tempMazeImg.getAbsolutePath))
       }
     )
 
@@ -107,9 +115,6 @@ class HomeController @Inject()(controllerComponents: ControllerComponents) exten
     val tempMazeImg = File.createTempFile("mazeImg", ".png")
     ImageIO.write(mazeImg, "png", tempMazeImg)
 
-    file.Files.move(file.Paths.get(tempMazeImg.getAbsolutePath), file.Paths.get("TEST.png"), file.StandardCopyOption.REPLACE_EXISTING)
-
-    //ImageIO.write(tempMazeImg, "png", new File("tempMazeImg.png"))
     Ok(views.html.index(mazeForm, modifiedMazeData, tempMazeImg.getAbsolutePath))
   }
 }
