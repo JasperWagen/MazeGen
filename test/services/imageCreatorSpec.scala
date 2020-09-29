b1c70d9cf828c4612e78776a091b7ec6888733a2
JasperWagen <jasperwagen@outlook.com>
2020-09-29 11:03:21 +0100

    resolving test issues

--- test/services/imageCreatorSpec.scala
+++ test/services/imageCreatorSpec.scala
@@ -1,5 +1,5 @@
 package services
-
+import models.mazeInfo.MazeRequestData
 import java.awt.image.BufferedImage
 
 import org.scalatest.funsuite.AnyFunSuite
@@ -9,18 +9,19 @@ import scala.collection.mutable.ListBuffer
 
 class imageCreatorSpec extends AnyFunSuite{
     val arrayToImgTest = new ArrayToImage
+    val testRequestData = MazeRequestData()
     test("Create blank buffered image"){
         val testArray = Array.ofDim[Byte](100,100)
-        assert(arrayToImgTest.imgInit(testArray, 13158600, 20).isInstanceOf[BufferedImage])
+        assert(arrayToImgTest.imgInit(testArray, testRequestData).isInstanceOf[BufferedImage])
     }
 
     test("Test image height dimension"){
         val testArray = Array.ofDim[Byte](100,150)
-        assert(arrayToImgTest.imgInit(testArray, 13158600, 20).getHeight == testArray.length*20)
+        assert(arrayToImgTest.imgInit(testArray, testRequestData).getHeight == testArray.length*20)
     }
 
     test("Test image width dimension"){
         val testArray = Array.ofDim[Byte](100,150)
-        assert(arrayToImgTest.imgInit(testArray, 13158600, 20).getWidth == testArray(0).length*20)
+        assert(arrayToImgTest.imgInit(testArray, testRequestData).getWidth == testArray(0).length*20)
     }
 }
